307b586c222af4837a6b58949ff89a8a491658be
willin <willin@willin.org>
2018-12-13 13:45:10 +0800

    add random list

--- app/pages/hot/hot.js
+++ app/pages/hot/hot.js
@@ -2,12 +2,14 @@
 import regeneratorRuntime from '../../lib/runtime.js';
 import { formatDate } from '../../lib/common.js'
 
+const SEED = 1e6;
+
 Page({
   data: {
     index: 0,
     page: 1,
     pages: 1,
-    orderType: ['按热度降序', '按更新时间降序', '按发布时间降序'],
+    orderType: ['按热度降序', '按发布时间降序', '随机推荐'],
     hot: []
   },
   onPullDownRefresh: function () {
@@ -23,9 +25,9 @@ Page({
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      index: e.detail.value
+      index: parseInt(e.detail.value, 10)
     })
-    this.discover(1, e.detail.value)
+    this.discover(1, parseInt(e.detail.value, 10))
       .then(this.prepareData);
   },
   onLoad: function () {
@@ -41,19 +43,30 @@ Page({
     const skip = (page - 1) * limit;
 
     const db = wx.cloud.database();
-    const { data: result = []} = order === 1 
-      ? await db.collection('comments').skip(skip).limit(limit).get()
-      : await db.collection('comments').skip(skip).limit(limit).orderBy(order === 0 ? 'liked' : 'time', 'desc').get();
-   
-    wx.hideLoading();
-    console.log(result);
-    const { total } = await db.collection('comments').count();
-    const pages = Math.ceil(total / limit);
-    this.setData({
-      pages,
-      page
-    });
-    return result;
+    if(order === 2){
+      const { data: result = [] } =  await db.collection('comments').where({
+        liked: db.command.gt(Math.round(Math.random() * SEED))
+      }).limit(limit).orderBy(order === 0 ? 'liked' : 'time', 'desc').get();
+      wx.hideLoading();
+      console.log(result);
+      this.setData({
+        pages: 10,
+        page: 1
+      });
+      return result;
+    } else {
+      const { data: result = [] } = await db.collection('comments').skip(skip).limit(limit).orderBy(`${order}` === '0' ? 'liked' : 'time', 'desc').get();
+
+      wx.hideLoading();
+      console.log(result);
+      const { total } = await db.collection('comments').count();
+      const pages = Math.ceil(total / limit);
+      this.setData({
+        pages,
+        page
+      });
+      return result;
+    }
   },
   prepareData: function (hot) {
     this.setData({
@@ -65,14 +78,17 @@ Page({
     return hot;
   },
   prepareDataAppend: function (hot) {
-    this.setData({
-      hot: this.data.hot.concat(
-        JSON.parse(JSON.stringify(hot)).map(x => Object.assign(x, {
+    const newHot = this.data.hot.concat(
+      JSON.parse(JSON.stringify(hot))
+        .filter(x => this.data.hot.findIndex(y => x.cid === y.cid) === -1)
+        .map(x => Object.assign(x, {
           created: formatDate('yy年M月d日 h时', x.time),
           likedTip: x.liked > 10000 ? `${Math.round(x.liked / 1000) / 10}万` : x.liked
         }))
-      )
+    );
+    this.setData({
+      hot: newHot
     });
-    return hot;
+    return newHot;
   }
 })
